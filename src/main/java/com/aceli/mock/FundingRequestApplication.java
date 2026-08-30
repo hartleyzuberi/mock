592ae5726bc99eda33b4a7b06463d52015906799
hartleyzuberi <hartleyzuberi@gmail.com>
2026-08-30 09:38:16 +0300

    feat: add Spring Boot application entry point

---
+++ src/main/java/com/aceli/mock/FundingRequestApplication.java
@@ -0,0 +1,11 @@
+package com.aceli.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundingRequestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FundingRequestApplication.class, args);
+    }
+}
